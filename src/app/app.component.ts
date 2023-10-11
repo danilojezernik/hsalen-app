d0f96523315297971f6bea5d11e4d5686065c0ba
danilojezernik <dani.jezernik@gmail.com>
2023-10-11 19:42:19 +0200

    add(): show go back only on admin pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,27 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NavigationEnd, Router} from "@angular/router";
+import {filter} from "rxjs";
+
+const routesToShowGoBack: string[] = ['/blog-pregled', '/blog/edit/:id', '/mediji-pregled', '/mediji/edit/:id', '/events', '/email-pregled'];
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html'
+  selector: 'app-root',
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-    title = 'hypnosis-studio-alen-app';
+export class AppComponent implements OnInit {
+
+  shouldShowGoBack: boolean = true;
+
+  constructor(private router: Router) {
+  }
+
+  ngOnInit() {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      this.shouldShowGoBack = routesToShowGoBack.includes(this.router.url);
+    });
+  }
+
+
 }
