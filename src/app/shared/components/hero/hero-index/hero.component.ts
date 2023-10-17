257e3cb5bf372f273f96e73732ab4047dc631310
danilojezernik <dani.jezernik@gmail.com>
2023-10-17 21:53:40 +0200

    add(): scroll to effect to a header button

--- src/app/shared/components/hero/hero-index/hero.component.ts
+++ src/app/shared/components/hero/hero-index/hero.component.ts
@@ -8,4 +8,20 @@ export class HeroComponent {
 
   @Input() hero: { naslov: string, podnaslov: string } | undefined
 
+  // Method to scroll to the 'app-offer' component
+  scrollToOffer() {
+    const offerElement = document.querySelector('app-offer');
+
+    if (offerElement) {
+      // Get the offset top of the element, including the margin
+      const offsetTop = offerElement.getBoundingClientRect().top + window.pageYOffset;
+
+      // Scroll slightly above the element (adding 40px margin)
+      window.scrollTo({
+        top: offsetTop - 140,
+        behavior: 'smooth'
+      });
+    }
+  }
+
 }
