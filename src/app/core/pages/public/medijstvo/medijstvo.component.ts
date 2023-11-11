2b70ee438037d259c8afe4d86688c0794d819299
danilojezernik <dani.jezernik@gmail.com>
2023-11-11 22:24:20 +0100

    add(): comment

--- src/app/core/pages/public/medijstvo/medijstvo.component.ts
+++ src/app/core/pages/public/medijstvo/medijstvo.component.ts
@@ -1,3 +1,4 @@
+// Import necessary modules and components
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Router} from "@angular/router";
@@ -19,13 +20,17 @@ export class MedijstvoComponent implements OnInit {
   }
 
   ngOnInit() {
+    // Define the path to the JSON data file
     const path: string = 'assets/medijstvo.json'
+
+    // Fetch data from the JSON file using HttpClient
     this.db.get(path).subscribe(response => {
+      // Store the fetched data in the 'medijstvo' property
       this.medijstvo = response;
 
+      // Populate the 'heroData' object with data from 'medijstvo'
       this.heroData.naslov = this.medijstvo.naslov
       this.heroData.path = this.router.url.slice(1);
     })
   }
-
 }
